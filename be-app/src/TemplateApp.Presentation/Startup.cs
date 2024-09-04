096bb143c7b07374b585cecafb5cb0a0eaebfe97
Konstantin Yudin <dev.kyudin@gmail.com>
2024-09-04 22:37:50 +0300

    Update dotnet version

--- be-app/src/TemplateApp.Presentation/Startup.cs
+++ be-app/src/TemplateApp.Presentation/Startup.cs
@@ -4,14 +4,9 @@ using TemplateApp.Infrastructure;
 
 namespace TemplateApp.Presentation;
 
-public class Startup
+public class Startup(IConfiguration configuration)
 {
-	public Startup(IConfiguration configuration)
-	{
-		Configuration = configuration;
-	}
-
-	public IConfiguration Configuration { get; }
+	public IConfiguration Configuration { get; } = configuration;
 
 	public void ConfigureServices(IServiceCollection serviceCollection)
 	{
